f341babfb18a810889d44d7e792fb9a5205dea1f
sukesh <37667509+nagasukesh@users.noreply.github.com>
2020-09-15 13:10:29 +0530

    Update strpat.py

--- strpat.py
+++ strpat.py
@@ -2,24 +2,29 @@ pattern_list=[]
 string_pattern={}
 
 #takes input for the pattern
-pattern=input("Enter the pattern")
+pattern=input("Enter the pattern\n")
 
 #takes input for the string
-string=input("Enter the string")
+string=input("Enter the string\n")
 
 def pattern_matching(string,pattern) :
     string=string.split(" ")
     if len(pattern) != len(string) :
-        return print(False)
+        return 0
     for i in range(len(string)) :
         if string[i] not in string_pattern.keys() :
             if pattern[i] in pattern_list :
-                return print(False)
+                return 0
             pattern_list.append(pattern[i])
             string_pattern[string[i]]=pattern[i]
         else :
             if pattern[i] != string_pattern[string[i]] :
-                return print("False")
-    return print(True)
+                return 0
+    return 1
 
-pattern_matching(string,pattern)
+pat_str=pattern_matching(string,pattern)
+
+if pat_str == 1 :
+    print(True)
+else :
+    print(False)
